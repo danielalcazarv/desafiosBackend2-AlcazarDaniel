a7e506445176b235ed4e259b8ab18567413a77fe
Daniel Alcazar <danielalcazar54@gmail.com>
2022-06-28 00:36:24 -0300

    MOD metodos getall y save funcionales

--- desafio-2.js
+++ desafio-2.js
@@ -1,6 +1,59 @@
 var fs = require('fs');
 
 class Contenedor {
+    constructor(archivo){
+        this.archivo = archivo
+    }
+
+    async getAll() {
+        try{
+            const contenido = await fs.promises.readFile(this.archivo,'utf-8')
+            if (contenido===""){
+                const datos = [];
+                return datos;
+            }else{
+                const datos2 = JSON.parse(contenido);
+                return datos2;
+            }
+        }
+        catch (error){
+            console.log(error)
+        }
+    }
+
+    async save (objeto) {
+        const objsCollection = await this.getAll()
+        let newId
+        if (objsCollection.length == 0) {
+            newId = 1
+        } else {
+            newId = objsCollection[objsCollection.length - 1].id + 1
+        }
+        const newProducto = {id:newId, ...objeto};
+        objsCollection.push(newProducto);
+        const objsJson = JSON.stringify(objsCollection);
+        try {
+            await fs.promises.writeFile(this.archivo,objsJson);
+            console.log("Producto guardado.")
+        }
+        catch (error){
+            console.log("Error al guardar el producto. Tipo de error: "+error)
+        }
+    }
+}
+
+const productos = new Contenedor ('productos.txt')
+
+//Los productos se ingresan de uno a la vez.
+//productos.save({titulo:"tijera",precio:123,url:"https/www.librería.com.ar/asdfasfasfas"})
+//productos.save({titulo:"escuadra",precio:200,url:"https/www.librería.com.ar/134123asfas"})
+productos.save({titulo:"lapicera",precio:354,url:"https/www.librería.com.ar/145dsaafj89a"})
+
+
+
+
+/*
+class Contenedor {
     constructor(nombre, precio, thumbnail){
         this.nombre=nombre;
         this.precio=precio;
@@ -81,9 +134,9 @@ productos.push(new Contenedor("birome",68.28,"link/brrardasgaasñ"))
 productos.push(new Contenedor("resma",422.32,"link/asd2345adfsa"))
 productos.push(new Contenedor("borrador",116.28,"link/asd2345adfsa"))
 
-save(productos);
-getById(1);
-getAll()
-deleteById(4)
+//save(productos);
+//getById(1);
+//getAll()
+//deleteById(4)
 //Comente el delete para poder mostrar el resto de las funciones.
-//deleteAll()
+deleteAll()*/
